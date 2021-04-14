77b14cf9b091c0a383d5ddbfd4a653b8d2359578
Rishad pt <rishadpt16@gmail.com>
2021-04-14 15:01:05 +0530

    temp saving

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const hbs = require('express-handlebars')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
