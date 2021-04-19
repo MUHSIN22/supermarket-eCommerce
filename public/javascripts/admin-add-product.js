faea176f59c2fb44a485dedd600e49fb46eb9118
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-04-19 14:20:17 +0530

    admin page route and some funcions are added

---
+++ public/javascripts/admin-add-product.js
@@ -0,0 +1,50 @@
+
+$('document').ready(function () {
+    let defaultPricing = $('#default-pricing');
+    let customPricing = $('#custom-pricing');
+    let defaultPricingContainer = $('.product-price-container');
+    let customPricingContainer = $('.product-custom-prize-container')
+    let categoryList = $('#category-list');
+    let categoryButton = $('#product-category')
+    let pricingSwitch = (type, onContainer, offContainer) => {
+        type.on('click', function () {
+            offContainer.css('display', 'none')
+            onContainer.css('display', 'flex')
+        })
+    }
+    categoryButton.on('click',function(e){
+        e.preventDefault();
+        categoryList.css('display','block');
+    })
+    categoryList.find('li').on('click',function(event){
+        var value = $(event.target).html()
+        categoryButton.html('Category : '+value)
+        $('.category-save').val(value)
+        categoryList.css('display','none')
+    })
+    $
+    pricingSwitch(defaultPricing, defaultPricingContainer, customPricingContainer);
+    pricingSwitch(customPricing, customPricingContainer, defaultPricingContainer);
+    
+    $('#images').on('change',function(){
+        var totalImages = $('.images').length;
+        for(var i=0;i<totalImages;i++){
+            $('#image-preview').append("<img src='"+URL.createObjectURL(event.target.files[i])+"'>");
+            $('#image-preview').append(`<input type="text" name="images" value="${ this.value }" hidden/>`)
+        }
+    })
+})
+
+$('.add-product-page-container').submit((event) => {
+    event.preventDefault();
+    const productDetails = $('.add-product-page-container').serialize()
+    $.ajax({
+        url:'/admin/add-product',
+        method : 'post',
+        data : productDetails,
+        success : (response)=>{
+
+        }
+    })
+})
+
--- routes/admin.js
+++ routes/admin.js
@@ -10,5 +10,11 @@ router.get('/admin-order',(req,res) =>{
   let heading = 'Order Details'
   res.render('admin/admin-order',{admin:true,heading})
 })
+router.get('/add-product',(req,res) => {
+  res.render('admin/admin-add-product',{admin:true})
+})
+router.post('/add-product',(req,res) => {
+  console.log(req.body);
+})
 module.exports = router;
 
